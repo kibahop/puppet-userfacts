57f2bfde40805bd998ab3ccc152de7fbe8591df1
Petri Lammi <petri.lammi@tietoteema.fi>
2018-04-10 13:06:44 +0300

    Add admingroupname fact
    Signed-off-by: Petri Lammi <petri.lammi@tietoteema.fi>

---
+++ facts.d/admingroupname.ps1
@@ -0,0 +1,10 @@
+﻿$locality = (Get-UICulture).Name
+if ($locality -eq 'fi-FI') {
+    $admingroup_name = 'Järjestelmänvalvojat'
+}
+else {
+    $admingroup_name = 'Administrators'
+}
+
+write-host ("admingroupname={0}" -f $user)
+
